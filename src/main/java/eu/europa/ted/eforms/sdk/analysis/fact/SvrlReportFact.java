9b1dde9678eadc6eaf1f87484f4dc3d7c8161c14
Bertrand Lorentz <bertrand.lorentz@publications.europa.eu>
2023-05-03 15:12:18 +0200

    SvrlReportFact: Fix indentation to use spaces

--- src/main/java/eu/europa/ted/eforms/sdk/analysis/fact/SvrlReportFact.java
+++ src/main/java/eu/europa/ted/eforms/sdk/analysis/fact/SvrlReportFact.java
@@ -6,32 +6,32 @@ import eu.europa.ted.eforms.sdk.domain.SvrlReport;
 
 public class SvrlReportFact implements SdkComponentFact<String> {
 
-	private final SvrlReport svrlReport;
+  private final SvrlReport svrlReport;
 
-	public SvrlReportFact(SvrlReport svrlReport) {
-		Validate.notNull(svrlReport, "SvrlReport is null");
-		this.svrlReport = svrlReport;
-	}
+  public SvrlReportFact(SvrlReport svrlReport) {
+    Validate.notNull(svrlReport, "SvrlReport is");
+    this.svrlReport = svrlReport;
+  }
 
-	public int getErrorCount() {
-		return svrlReport.getErrorCount();
-	}
+  public int getErrorCount() {
+    return svrlReport.getErrorCount();
+  }
 
-	/**
-	 * Returns true if the report is for an XML notice that is intended to be valid.
-	 */
-	public boolean shouldBeValid() {
-		return !svrlReport.getFilename().startsWith("INVALID");
-	}
+  /**
+   * Returns true if the report is for an XML notice that is intended to be valid.
+   */
+  public boolean shouldBeValid() {
+    return !svrlReport.getFilename().startsWith("INVALID");
+  }
 
-	@Override
-	public String getId() {
-		int extensionIndex = svrlReport.getFilename().lastIndexOf(".");
-		return svrlReport.getFilename().substring(0, extensionIndex);
-	}
+  @Override
+  public String getId() {
+    int extensionIndex = svrlReport.getFilename().lastIndexOf(".");
+    return svrlReport.getFilename().substring(0, extensionIndex);
+  }
 
-	@Override
-	public String getTypeName() {
-	  return "svrlReport";
-	}
+  @Override
+  public String getTypeName() {
+    return "svrlReport";
+  }
 }
