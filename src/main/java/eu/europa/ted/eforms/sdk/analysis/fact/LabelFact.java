b74193817669be6c0a8e3db39532a707aa949f74
Bertrand Lorentz <bertrand.lorentz@publications.europa.eu>
2023-06-09 17:15:23 +0200

    translations: Check for invalid characters (TEDEFO-1871)

--- src/main/java/eu/europa/ted/eforms/sdk/analysis/fact/LabelFact.java
+++ src/main/java/eu/europa/ted/eforms/sdk/analysis/fact/LabelFact.java
@@ -1,5 +1,8 @@
 package eu.europa.ted.eforms.sdk.analysis.fact;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import eu.europa.ted.eforms.sdk.analysis.domain.Label;
 import eu.europa.ted.eforms.sdk.analysis.domain.enums.Language;
 
@@ -23,6 +26,29 @@ public class LabelFact implements SdkComponentFact<String> {
 
   public String getText() {
     return label.getText();
+  }
+
+  private final boolean isInvalidCharacter(int codePoint) {
+    switch (Character.getType(codePoint)) {
+      case Character.CONTROL:
+      case Character.FORMAT:
+      case Character.PRIVATE_USE:
+      case Character.SURROGATE:
+      case Character.UNASSIGNED:
+        return true;
+      default:
+        return false;
+    }
+  }
+  
+  public Set<String> getInvalidCharacters() {
+    Set<String> invalidCharacters = new HashSet<>();
+
+    label.getText().codePoints().filter(c -> isInvalidCharacter(c)).forEach(c -> {
+      invalidCharacters.add(Character.toString(c));
+    });
+
+    return invalidCharacters;
   }
 
   @Override
