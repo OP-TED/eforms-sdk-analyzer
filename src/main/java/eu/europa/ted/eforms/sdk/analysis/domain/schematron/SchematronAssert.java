c50a2f1766d9232691ada043914d6c35ac5caf8f
Bertrand Lorentz <bertrand.lorentz@publications.europa.eu>
2026-03-16 14:38:45 +0100

    schematron: Update check on assert id for SDK 2 (TEDEFO-4972)
    In SDK 2, we can have the same assert identifier in different phases.
    So adapt the check accordingly, along with the unit test data.
    
    The same check is done for SDK 1, so it is now more permissive, which is
    OK.

--- src/main/java/eu/europa/ted/eforms/sdk/analysis/domain/schematron/SchematronAssert.java
+++ src/main/java/eu/europa/ted/eforms/sdk/analysis/domain/schematron/SchematronAssert.java
@@ -2,14 +2,20 @@ package eu.europa.ted.eforms.sdk.analysis.domain.schematron;
 
 public class SchematronAssert {
   private final String id;
+  private final String patternId;
   private String diagnostics;
 
-  public SchematronAssert(String id) {
+  public SchematronAssert(String id, String patternId) {
     this.id = id;
+    this.patternId = patternId;
   }
 
   public String getId() {
     return id;
+  }
+
+  public String getPatternId() {
+    return patternId;
   }
 
   public String getDiagnostics() {
